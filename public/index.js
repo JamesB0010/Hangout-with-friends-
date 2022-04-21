5369ecc38deeaddb93297f7ad7556ebb429c35ce
JamesB0010 <98528984+JamesB0010@users.noreply.github.com>
2022-04-21 16:33:07 +0100

    fingers crossed

--- public/index.js
+++ public/index.js
@@ -44,7 +44,7 @@ canvas.addEventListener("touchstart", event =>{
   let evt = (typeof event.originalEvent === "undefined") ? event : event.originalEvent;
   console.log("iphone touch");
   let touch = evt.touches[0] || evt.changedTouches[0];
-  console.log(touch);
+  console.log(touch.pageX);
   if (!(event.clientX > rect.left && event.clientX < rect.right && event.clientY > rect.top && event.clientY < rect.bottom)){
     return
   }
